ec74a9693b44930575b7587cb8362f818e6b3d47
jiangdizhao <jiangdizhao@gmail.com>
2026-07-21 17:16:15 +1000

    Load the Phase 1 microphone cleanup guard

--- ui/smart-office-ui/src/main.tsx
+++ ui/smart-office-ui/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './voice/safeRealtimeAgentRuntime'
 import App from './App.tsx'
 import VoiceDebugPanel from './voice/VoiceDebugPanel.tsx'
 
